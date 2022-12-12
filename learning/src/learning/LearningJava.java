8de17d52e51e3a8428b4b45e5e69c4a9c25b44c9
FilipC93 <mfcekani@gmail.com>
2022-12-12 05:26:32 +0200

    Hello

---
+++ learning/src/learning/LearningJava.java
@@ -0,0 +1,11 @@
+package learning;
+
+public class LearningJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Filip";
+		System.out.println(name);
+	}
+
+}
---
+++ learning/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Φίλιππος
+ *
+ */
+module learning {
+}
