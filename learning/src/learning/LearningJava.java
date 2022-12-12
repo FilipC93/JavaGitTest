1ec7b823e9f6c95697b4af2997ab0698d55757a4
FilipC93 <mfcekani@gmail.com>
2022-12-12 05:35:04 +0200

    Mostly a change to see if it still works.

--- learning/src/learning/LearningJava.java
+++ learning/src/learning/LearningJava.java
@@ -5,7 +5,8 @@ public class LearningJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name = "Filip";
-		System.out.println(name);
+		int age = 29;
+		System.out.println("Hello, my name is " + name + " and I'm " + age + " years old.");
 	}
 
 }
